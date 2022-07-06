28c4d1089513ee9fce2d15666f72bd5baa53bdf4
Genesio S Pacehco <genesio.s.p.28@gmail.com>
2022-07-06 11:28:43 -0300

    Adding put route, edit button working

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -35,6 +35,17 @@ router.get('/new', async (req, res) => {
   }
 })
 
+router.get('/:id/edit', async (req, res) => {
+  try {
+    let checklist = await Checklist.findById(req.params.id)
+    res.status(200).render('checklists/edit', { checklist: checklist })
+  } catch (erro) {
+    res
+      .status(500)
+      .render('pages/error', { error: 'Erro ao editar o formulário' })
+  }
+})
+
 router.get('/:id', async (req, res) => {
   try {
     let checklist = await Checklist.findById(req.params.id)
@@ -47,17 +58,16 @@ router.get('/:id', async (req, res) => {
 })
 
 router.put('/:id', async (req, res) => {
-  let { name } = req.body
-
+  let { name } = req.body.checklist
+  let checklist = await Checklist.findById(req.params.id)
   try {
-    let checklist = await Checklist.findByIdAndUpdate(
-      req.params.id,
-      { name },
-      { new: true }
-    )
-    res.status(200).json(checklist)
+    await checklist.update({ name })
+    res.redirect('/checklists')
   } catch (error) {
-    res.status(422).json(error)
+    let errors = error.errors
+    res
+      .status(422)
+      .render('checklists/edit', { checklist: { ...checklist, errors } })
   }
 })
 
